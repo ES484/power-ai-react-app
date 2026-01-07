259f3cf30cb8c8bcad35017c206b710e187dd964
ES464 <esraasaad478@gmail.com>
2026-01-07 17:05:17 +0200

    run frontend and backend together

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+
+concurrently([
+    {
+        command: 'bun run dev',
+        name: 'client',
+        prefixColor: 'cyan',
+        cwd: 'packages/client',
+    },
+    {
+        command: 'bun run dev',
+        name: 'server',
+        prefixColor: 'blue',
+        cwd: 'packages/server',
+    },
+]);
