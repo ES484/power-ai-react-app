2a8fc731171ad9e77a19922977b61f05179ad36f
ES464 <esraasaad478@gmail.com>
2026-01-08 12:35:20 +0200

    tailwind - shadcn ui - prettier setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from 'react';
-import './App.css'
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('');
-  useEffect(() => {
-    fetch('/api/hello')
-    .then(res => res.json())
-    .then(data => setMessage(data.message))
-  }, [])
+    const [message, setMessage] = useState('');
+    useEffect(() => {
+        fetch('/api/hello')
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
+    }, []);
 
-  return (
-    <>
-    Hello: {message}
-    </>
-  )
+    return (
+        <>
+            <p className="text-red-900">Hello: {message}</p>
+            <Button>Test</Button>
+        </>
+    );
 }
 
-export default App
+export default App;
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-      },
-    }
-  },
-})
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
